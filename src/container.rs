e1e5fe21076a12b415d63a157fe0f6f199a5b3c9
sanix-darker <s4nixd@gmail.com>
2024-07-13 00:38:52 +0200

    feat: add filesystem management on container level

--- src/container.rs
+++ src/container.rs
@@ -1,10 +1,15 @@
-use crate::cgroups;
+use crate::{cgroups, filesystem};
 
-pub fn start_container(image: &str){
-    // cgroups
+pub fn start_container(image: &str) {
+    println!("Starting container {:?}", image);
+    // > cgroups
     // since dak cgroup is already created (manualy)no need to create it again
-    cgroups::attach_dak_cgroup()
+    // we just set the memory max
+    cgroups::set_memory_max(b"100");
+
+    // > filesystem
+    filesystem::mount_overlayfs();
+
     // networking
-    // filesystem
     // namespaces
 }
---
+++ src/filesystem.rs
@@ -0,0 +1,27 @@
+use std::ffi::CString;
+use nix::mount::{mount, MsFlags};
+
+pub fn mount_overlayfs(){
+    let lowerdir =  "./lower";
+    let upperdir =  "./upper";
+    let workdir =  "./work";
+    let mergedir =  "./merge";
+
+    std::fs::create_dir_all(lowerdir).expect("Failed to create lowerdir");
+    std::fs::create_dir_all(upperdir).expect("Failed to create upperdir");
+    std::fs::create_dir_all(workdir).expect("Failed to create workdir");
+    std::fs::create_dir_all(mergedir).expect("Failed to create mergedir");
+
+    let options = format!(
+        "lowerdir={},upperdir={},workdir={}",
+        lowerdir, upperdir, workdir,
+    );
+
+    mount(
+        Some("overlay"),
+        mergedir,
+        Some("overlay"),
+        MsFlags::empty(),
+        Some(&*CString::new(options).unwrap())
+    ).expect("Failed to mount overlay filesystem");
+}
