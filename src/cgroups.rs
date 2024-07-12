7ecc5518ba683e0db703a536d254df060ee0aac5
sanix-darker <s4nixd@gmail.com>
2024-07-13 00:37:18 +0200

    feat: refacto cgroup interface for set... resources

--- src/cgroups.rs
+++ src/cgroups.rs
@@ -1,14 +1,23 @@
 use std::{fs::OpenOptions, io::Write};
 
-const DAK_CGROUP_NAME: &str = "dak.cgroup";
+const DAK_CGROUP_PATH: &str = "/sys/fs/cgroup/dak.cgroup";
 
-// TODO: pass a process id/name to attach to the cgroups.
-pub fn attach_dak_cgroup(){
-    let path = format!("/sys/fs/cgroup/{}", DAK_CGROUP_NAME);
+/// To set memory max
+/// Usage:
+/// cgroups::set_memory_max(b"100")
+pub fn set_memory_max(memory_in_mb: &[u8]) {
+    let memory_limit_path = format!("{}/memory.max", DAK_CGROUP_PATH);
 
-    println!("Attach specs to cgroups {:?}...", path);
+    println!(
+        "Set memory max to {:?}... in {}",
+        memory_in_mb, memory_limit_path,
+    );
 
-    let memory_limit_path = format!("{}/memory.limit_in_bytes", path);
-    let mut memory_limit = OpenOptions::new().write(true).open(memory_limit_path).unwrap();
-    memory_limit.write_all(b"1000").unwrap();
+    let mut memory_limit = OpenOptions::new()
+        .write(true)
+        .open(memory_limit_path)
+        .unwrap();
+    memory_limit.write_all(memory_in_mb).unwrap();
 }
+
+// TODO: set cpu max
